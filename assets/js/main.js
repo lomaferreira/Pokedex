80f3c1ebf982283b3d52a28f68d1e578641b8c4e
lomaferreira <lomasantos666@gmail.com>
2023-09-12 22:41:44 -0300

    integrating PokeAPI

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log("foiii");
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+  //acontece quando a promise é cumprida
+  .then((response) => response.json())
+  //pega dentro do json o results(lista de pokemons)
+  .then((jsonBody) => jsonBody.results)
+  //recebe o return do jsonBody()
+  .then((pokemonList) => {
+    debugger;
+    console.log(pokemonList);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
