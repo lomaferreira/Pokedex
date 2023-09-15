cfcc7f3fa4c1541ca788e822a564ddbe1ac5c839
lomaferreira <lomasantos666@gmail.com>
2023-09-15 16:30:45 -0300

    converting PokeApi framework  to the model below

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,15 @@
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon">
-      <span class="number">#001</span>
+    <li class="pokemon ${pokemon.type}">
+      <span class="number">#${pokemon.number}</span>
       <span class="name">${pokemon.name}</span>
         <div class="details">
           <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+          ${pokemon.types
+            .map((type) => `<li class="type ${type}">${type}</li>`)
+            .join("")}
           </ol>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+          <img src="${pokemon.photo}"
             alt="${pokemon.name}">
         </div>
     </li>
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,31 @@
 const pokeApi = {};
 
+//convertendo o modelo da estrutura do PokeApi para o modelo abaixo
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.order;
+  pokemon.name = pokeDetail.name;
+
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type]=types
+
+  pokemon.types = types;
+  pokemon.type = type;
+
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+return pokemon
+}
+
+
+//Requisição de  detalhes do pokemon
 pokeApi.getPokemonDetail = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
 };
 
+//Requisiçâo da PokeApi
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
   return (
